a46c15c663a969cd4e4972f50a4224cbeb4eac3e
Bismark Agyare <bismarkagyare54@gmail.com>
2023-04-05 11:18:24 +0000

    create a todo class for individual todos

---
+++ src/todo.js
@@ -0,0 +1,25 @@
+export class Todo {
+  constructor(title, description, dueDate, priority, completed = false) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.completed = completed;
+  }
+
+  setTitle(title) {
+    this.title = title;
+  }
+
+  getTitle() {
+    return this.title;
+  }
+
+  setDueDate(dueDate) {
+    this.dueDate = dueDate;
+  }
+
+  getDueDate() {
+    return this.dueDate;
+  }
+}
