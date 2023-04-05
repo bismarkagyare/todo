749c213406acc97ab9de0186d8daac2006843c0b
Bismark Agyare <bismarkagyare54@gmail.com>
2023-04-05 10:24:43 +0000

    add some methods to projects class

--- src/projects.js
+++ src/projects.js
@@ -4,4 +4,40 @@ export class Projects {
     this.title = title;
     this.todos = [];
   }
+
+  setTitle(title) {
+    this.title = title;
+  }
+
+  getTitle() {
+    return this.title;
+  }
+
+  addTodo(todo) {
+    this.todos.push(todo);
+  }
+
+  editTodoById(id, updatedProperties) {
+    this.todos = this.todos.map((todo) => {
+      if (todo.id === id) {
+        return Object.assign({}, todo, updatedProperties);
+      } else {
+        return todo;
+      }
+    });
+  }
+
+  deleteTodoById(id) {
+    const updatedTodos = this.todos.filter((todo) => {
+      return todo.id !== id;
+    });
+    this.todos = updatedTodos;
+  }
+
+  filterCompletedTodos() {
+    return this.todos.filter(todo => {
+      todo.completed === true;
+    });
+  }
+
 }
