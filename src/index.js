d59eeb522c8d5a99287c8139337e8c67723d505a
Bismark Agyare <bismarkagyare54@gmail.com>
2023-04-06 10:42:37 +0000

    remove console.logs and import addProject function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import { Projects } from "./projects";
 import { Todo } from "./todo";
+import addProject from "./dom";
 
-const myProject = new Projects('cook');
-const newTodo = new Todo('rice', 'cook rice', '05/04/2023', 'high', true);
-
-myProject.addTodoToProject(newTodo);
-console.log(myProject.getAllTodos());
+addProject();
