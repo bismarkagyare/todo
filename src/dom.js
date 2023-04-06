bc8d138c44a3bd0be9e49f717f0f5079c3a1a4b5
Bismark Agyare <bismarkagyare54@gmail.com>
2023-04-06 18:07:14 +0000

    remove modal on window click

--- src/dom.js
+++ src/dom.js
@@ -1,26 +1,36 @@
 function addProject() {
   const addProjectBtn = document.querySelector('.add-project-btn');
   addProjectBtn.addEventListener('click', addProjectModal);
+  window.addEventListener('click', (e) => {
+    if (e.target.classList.contains('add-project-modal-wrapper')) {
+      const removeModal = document.querySelector('.add-project-modal');
+      removeModal.remove();
+      document.body.style.overflow = '';
+    }
+  });  
 }
+
 
 function addProjectModal() {
   const addProjectModalContainer = document.createElement('div');
   addProjectModalContainer.classList.add('add-project-modal');
 
   addProjectModalContainer.innerHTML += ` 
-    <div class="add-project-modal-content">
-      <h1 class="add-project-title">add project</h1>
-      <input type="text" class="input-project-modal" id="input-project-modal">
-      <div class="project-modal-btns">
-        <button class="btn-add-modal">add</button>
-        <button class="btn-cancel-modal">cancel</button>
+    <div class="add-project-modal-wrapper">
+      <div class="add-project-modal-content">
+        <h1 class="add-project-title">add project</h1>
+        <input type="text" class="input-project-modal" id="input-project-modal">
+        <div class="project-modal-btns">
+          <button class="btn-add-modal">add</button>
+          <button class="btn-cancel-modal">cancel</button>
+        </div>
+      </div>
     </div>
-    `;
-  
+  `;
+
   const projectContainer = document.querySelector('.add-project-container');
   projectContainer.appendChild(addProjectModalContainer);
-  const projectModal = document.querySelector('.add-project-modal');
-  projectModal.classList.add('show');
+
 }
 
 export default addProject;
