04898969ceaf193b462d456ab8be0df9531c18b6
Bismark Agyare <bismarkagyare54@gmail.com>
2023-04-07 14:59:30 +0000

    use classes for the modal

--- src/dom.js
+++ src/dom.js
@@ -1,36 +1,40 @@
-function addProject() {
-  const addProjectBtn = document.querySelector('.add-project-btn');
-  addProjectBtn.addEventListener('click', addProjectModal);
-  window.addEventListener('click', (e) => {
-    if (e.target.classList.contains('add-project-modal-wrapper')) {
-      const removeModal = document.querySelector('.add-project-modal');
-      removeModal.remove();
-      document.body.style.overflow = '';
-    }
-  });  
-}
 
+export default class AddProject {
+  static addProject() {
+    const addProjectBtn = document.querySelector('.add-project-btn');
+    addProjectBtn.addEventListener('click', this.showModal);
 
-function addProjectModal() {
-  const addProjectModalContainer = document.createElement('div');
-  addProjectModalContainer.classList.add('add-project-modal');
+    window.addEventListener('click', (e) => {
+      if (e.target.classList.contains('add-project-modal-wrapper')) {
+        this.removeModal();
+      }
+    });
+  }
 
-  addProjectModalContainer.innerHTML += ` 
-    <div class="add-project-modal-wrapper">
-      <div class="add-project-modal-content">
-        <h1 class="add-project-title">add project</h1>
-        <input type="text" class="input-project-modal" id="input-project-modal" placeholder="Enter Project Name">
-        <div class="project-modal-btns">
-          <button class="btn-add-modal btn">add</button>
-          <button class="btn-cancel-modal btn">cancel</button>
+  static showModal() {
+    const addProjectModalContainer = document.createElement('div');
+    addProjectModalContainer.classList.add('add-project-modal');
+
+    addProjectModalContainer.innerHTML += ` 
+      <div class="add-project-modal-wrapper">
+        <div class="add-project-modal-content">
+          <h1 class="add-project-title">add project</h1>
+          <input type="text" class="input-project-modal" id="input-project-modal" placeholder="Enter Project Name">
+          <div class="project-modal-btns">
+            <button class="btn-add-modal btn">add</button>
+            <button class="btn-cancel-modal btn">cancel</button>
+          </div>
         </div>
       </div>
-    </div>
-  `;
+    `;
 
-  const projectContainer = document.querySelector('.add-project-container');
-  projectContainer.appendChild(addProjectModalContainer);
+    const projectContainer = document.querySelector('.add-project-container');
+    projectContainer.appendChild(addProjectModalContainer);
+  }
 
+  static removeModal() {
+    const removeModal = document.querySelector('.add-project-modal');
+    removeModal.remove();
+    document.body.style.overflow = '';
+  }
 }
-
-export default addProject;
